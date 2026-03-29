796b4ae6dda9d4013e4939890f333df508b95f20
wyonghui123 <onthemars@sina.cn>
2026-03-29 15:13:12 +0800

    Add src/composables/useMermaid/syntax/index.js

---
+++ src/composables/useMermaid/syntax/index.js
@@ -0,0 +1,4 @@
+export { useBusinessObjectSyntax } from './useBusinessObjectSyntax.js'
+export { useServiceModuleSyntax } from './useServiceModuleSyntax.js'
+export { COLOR_SCHEMES, getColors, assignColorsToGroups, getLinkColor } from './useMermaidColors.js'
+export { useBlockDiagramSyntax } from './useBlockDiagramSyntax.js'
