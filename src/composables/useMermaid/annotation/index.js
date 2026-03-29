9576ca21878104458f34b2fce88b84bfc262223b
wyonghui123 <onthemars@sina.cn>
2026-03-29 15:13:18 +0800

    Add src/composables/useMermaid/annotation/index.js

---
+++ src/composables/useMermaid/annotation/index.js
@@ -0,0 +1,14 @@
+/**
+ * 备注功能模块导出
+ */
+export { useAnnotation } from './useAnnotation.js';
+export { useAnnotationOverlay } from './annotationOverlay.js';
+export { 
+  CATEGORY_CONFIG, 
+  TYPE_CONFIG, 
+  PANEL_POSITION, 
+  DEFAULT_CATEGORY,
+  getCategoryConfig,
+  getTypeConfig,
+  toCircleNumber 
+} from './annotationConfig.js';
